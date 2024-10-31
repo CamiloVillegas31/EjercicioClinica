c8951ea6dd722a6e7fc2e183e01b9d597a575205
Camilo Andres Villegas Flor <162384786+CamiloVillegas31@users.noreply.github.com>
2024-10-31 11:44:09 -0500

    Cambios
    Cambios

---
+++ ClinicaMeMuero/src/clinicamemuero/ClinicaMeMuero.java
@@ -0,0 +1,26 @@
+/* 
+Integrantes
+
+Bryam Estiven Benachi Luligo
+Everson Castro Ramos
+Daniel Estiben Medina Tobar
+Camilo Andrés Villegas Flor
+
+Corporación Universitaria Unicomfacauca.
+Ing. de sistemas-Diurno
+
+*/
+package clinicamemuero;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class ClinicaMeMuero {
+
+
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
